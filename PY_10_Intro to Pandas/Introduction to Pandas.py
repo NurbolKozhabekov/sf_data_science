b29056302dfb4d3c851d7a48b823f73f50c67ea7
Nurbol Kozhabekov <nick.richeezy@gmail.com>
2025-06-08 16:47:08 +0400

    adding all the files related to completed sections of the course

---
+++ PY_10_Intro to Pandas/Introduction to Pandas.py
@@ -0,0 +1,9 @@
+import pandas as pd
+pd.__version__
+
+countries = pd.Series(
+    data = ['Англия', 'Канада', 'США', 'Россия', 'Украина', 'Беларусь', 'Казахстан'],
+    index = ['UK', 'CA', 'US', 'RU', 'UA', 'BY', 'KZ'],
+    name = 'countries'
+)
+displgay(countries)
